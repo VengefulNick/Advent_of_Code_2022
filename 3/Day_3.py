fdbe8853f4951418572fce0f7fc04936e713ef60
Nick <89831583+VengefulNick@users.noreply.github.com>
2022-12-13 09:53:04 +0100

    Delete 3 directory

--- 3/Day_3.py
+++
@@ -1,6 +0,0 @@
-# To help prioritize item rearrangement, every item type can be converted to a priority:
-#    Lowercase item types a through z have priorities 1 through 26.
-#    Uppercase item types A through Z have priorities 27 through 52.
-
-file = open("rugsacks.txt", "r")
-
